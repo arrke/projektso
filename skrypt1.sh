8ec4e7d19492ce08e297da60bc01c0e9e315550d
arrke <areek99@gmail.com>
2021-04-15 14:57:09 +0200

    Skrypt tworzacy foldery - do testowania

--- skrypt1.sh
+++ skrypt1.sh
@@ -27,6 +27,9 @@ mkdir -p ~/proba1/kat2/folder2/2
 echo "proba" > ~/proba1/kat2/folder2/1.txt
 echo "proba3" > ~/proba1/kat2/folder1/3/3.txt
 echo "proba4" > ~/proba1/kat2/folder1/4/4.txt
+echo "probatxt" > ~/proba1/kat2/folder1/1.txt
+echo "probatxt" > ~/proba1/kat2/folder1/12.txt
+echo "probatxt" > ~/proba1/kat2/folder1/13.txt
 
 echo "probaUsuwania" > ~/proba1/kat2/folder2/2/4.txt
 echo "probaUsuwania" > ~/proba1/kat2/folder2/2/4.txt
